441c28e25c4828bd5068fcb4cd2334965d212057
vlsk3104 <vlsk3104@gmail.com>
2021-03-11 19:18:59 +0900

    記事更新処理と記事削除の作成

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -30,4 +30,14 @@ class ArticleController extends Controller
     {
         return view('articles.edit', ['article' => $article]);
     }
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
 }
