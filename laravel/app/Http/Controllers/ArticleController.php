f754fa2a3e1a4a4e87a10fe8d79593c8f6011fa9
vlsk3104 <vlsk3104@gmail.com>
2021-03-11 18:28:00 +0900

    コントローラーとモデルの編集

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -16,5 +18,12 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');
+    }
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
