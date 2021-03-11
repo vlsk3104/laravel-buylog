e642f1cc3847c657dc185658d254cc9a45e2c84d
vlsk3104 <vlsk3104@gmail.com>
2021-03-11 19:04:16 +0900

    記事更新画面と記事削除モーダル画面の表示

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -26,4 +26,8 @@ class ArticleController extends Controller
         $article->save();
         return redirect()->route('articles.index');
     }
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
 }
