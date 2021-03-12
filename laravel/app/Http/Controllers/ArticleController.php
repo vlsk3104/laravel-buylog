631c4f03a3d36bc64cb3a4240a09810f4ca926c7
vlsk3104 <vlsk3104@gmail.com>
2021-03-12 15:23:29 +0900

    記事投稿処理とタグモデルの編集

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Tag;
+
 use App\Http\Requests\ArticleRequest;
 
 use Illuminate\Http\Request;
@@ -29,6 +31,11 @@ class ArticleController extends Controller
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
+
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
         return redirect()->route('articles.index');
     }
     public function edit(Article $article)
