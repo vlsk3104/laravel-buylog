997051874bc81f76eb81ea6c8a1c5786842dcc19
vlsk3104 <vlsk3104@gmail.com>
2021-03-12 18:41:39 +0900

    タグごとの記事一覧画面を作る

--- laravel/app/Tag.php
+++ laravel/app/Tag.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 class Tag extends Model
 {
     protected $fillable = [
@@ -13,5 +15,10 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
     }
 }
