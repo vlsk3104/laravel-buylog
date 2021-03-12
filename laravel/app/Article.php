7cf64930386676ecbdb556776a372ae8d8da241d
vlsk3104 <vlsk3104@gmail.com>
2021-03-12 12:32:08 +0900

    ユーザーが記事をいいね済みかを判定する

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Article extends Model
 {
@@ -17,5 +19,17 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
