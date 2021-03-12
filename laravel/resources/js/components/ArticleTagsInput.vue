f382756260e783e0e155fa4d61b6d57260bcdc92
vlsk3104 <vlsk3104@gmail.com>
2021-03-12 14:52:08 +0900

    入力されたタグをBladeからPOST送信可能にする

--- laravel/resources/js/components/ArticleTagsInput.vue
+++ laravel/resources/js/components/ArticleTagsInput.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
+        <input type="hidden" name="tags" :value="tagsJson" />
+
         <vue-tags-input
             v-model="tag"
             :tags="tags"
+            placeholder="タグを5個まで入力できます"
             :autocomplete-items="filteredItems"
             @tags-changed="newTags => (tags = newTags)"
         />
@@ -46,6 +49,10 @@ export default {
                     i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1
                 );
             });
+        },
+
+        tagsJson() {
+            return JSON.stringify(this.tags);
         }
     }
 };
