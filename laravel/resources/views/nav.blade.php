b3ff24ffb67db013d2f9f58214161d38c11262f0
vlsk3104 <vlsk3104@gmail.com>
2021-03-13 23:28:46 +0900

    fix

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
 
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>BuyLog</a>
 
   <ul class="navbar-nav ml-auto">
 
